52484b207489d4ad52c64e30615ba4ebc7dab502
Your Name <you@example.com>
2021-10-16 03:08:37 +0600

    added json file

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -1,172 +1,25 @@
+import { getDefaultNormalizer } from '@testing-library/dom';
 import React from 'react';
+import DeveloperList from '../DeveloperList/DeveloperList';
 import './Developer.css';
 
 const Developer = () => {
+    
     return (
-        <div className="container w-75">
-            <div className="grid-1">
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 30</p>
-                    <p>Rating:5</p>
-                    <p>permit:USD 1000</p>
-                    </div>
-                    <div className="text-center">
-                    <button className="rounded px-2">Hire Me</button>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="src/images/pic-1.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
-            <div className="developer">
-                <div className="p-3">
-                <div>
-                    <img src="././images/pic-3.jpg" alt="pic"></img>
-                </div>
-                    <h5>Name: Jhankar Mahbub</h5>
-                    <p>Profession: Owner of Programming Hero</p>
-                    <p>Skill:Web Developer and Software Engineer</p>
-                    <p>Age: 28</p>
-                    <p>Rating:5</p>
-                    <p>permit:1000$</p>
-                    </div>
-            </div>
+        <div>
+            <div className="row">
+                <div className="col-md-9">
 
+                    <DeveloperList />
+
+                </div>
+
+                <div className="col-md-3">
+                    {/* ekhane amra cart banabo */}
+                    <p>Eikhaney cart krbo</p>
+                </div>
             </div>
-            </div>
+        </div>
     );
 };
 
