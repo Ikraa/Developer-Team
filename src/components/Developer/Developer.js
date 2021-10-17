d2afe0675d6989f31e7f03d8d6bfdb5c63eed642
Your Name <you@example.com>
2021-10-18 03:25:55 +0600

    added data

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
 import DeveloperList from '../DeveloperList/DeveloperList';
 import './Developer.css';
 
 const Developer = () => {
+
+    const [developers, setDevelopers]=useState([])
+
+    useEffect(() =>{
+
+    fetch('/developer.json')
+    .then((res)=>res.json())
+    .then((data)=>setDevelopers(data));
+
+    } ,[])
     
     return (
         <div>
             <div className="row">
                 <div className="col-md-9">
-
-                    <DeveloperList />
-
+                <DeveloperList />
                 </div>
 
                 <div className="col-md-3">
